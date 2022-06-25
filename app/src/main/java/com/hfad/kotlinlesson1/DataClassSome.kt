792f2b6516f70babedf5b477d4d950d3b99efc60
Aleksey <meysonsib200189@mail.ru>
2022-06-25 16:00:48 +0700

    Kotlin урок - 1 | домашнее задание - 1

--- app/src/main/java/com/hfad/kotlinlesson1/DataClassSome.kt
+++ app/src/main/java/com/hfad/kotlinlesson1/DataClassSome.kt
@@ -1,3 +1,15 @@
 package com.hfad.kotlinlesson1
 
-data class DataClassSome(val name: String,val lastNam: String)
+data class DataClassSome(val name: String, val lastNam: String) { //первичный конст-р
+
+    var age: Int = 0
+
+    constructor(name: String, lastNam: String, age: Int) : this(name, lastNam) //вторичный конст-р
+
+    // В сравнении DataClass участвуют только парамметры переданные в основной конст-р
+    // var age: Int = 0 - это уже сравнить не получится
+}
+
+
+
+
--- app/src/main/java/com/hfad/kotlinlesson1/Male.kt
+++ app/src/main/java/com/hfad/kotlinlesson1/Male.kt
@@ -1,12 +1,7 @@
 package com.hfad.kotlinlesson1
 
 class Male : HumanInterface {
-    override fun work() {
-        super.work()
-    }
 
-    fun user() {
 
-    }
 
 }
